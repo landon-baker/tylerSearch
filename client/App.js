b3852a5bc1e1490fa1f87a9056ac38257c4ddc88
Landon <landon.baker@utexas.edu>
2019-07-15 13:04:08 -0500

    update

--- client/App.js
+++ client/App.js
@@ -15,40 +15,41 @@ class App extends React.Component{
     }
 
     handleChange(e){
-        console.log(e.currentTarget.value);
         e.preventDefault();
         //need to compare realtime keystrokes to filter thru names
         const names = this.state.items.map((item) => item.name);
-        console.log("Names", names);
-        names.filter((name) => {
-            if(name.includes(e.currentTarget.value)){
-                console.log("name", name);
-            }
-        })
         this.setState({ input: e.currentTarget.value });
     }
 
     handleSubmit(e){
-        
+        e.preventDefault();
+        window.dispatchEvent(new CustomEvent('productChanged', {
+            detail: {
+              id: this.state.input
+            }
+        }));
         //when ill render the page to the item thats clicked, or the exact item
     }
 
     componentWillMount(){
-        Axios.get('http://localhost:3001/items')
-        .then(response => {
-        this.setState({ items: response.data })
-        })
-        .catch(err => console.log('ERR', err));
+        // Axios.get('http://localhost:3001/items')
+        // .then(response => {
+        // this.setState({ items: response.data })
+        // })
+        // .catch(err => console.log('ERR', err));
+    }
+
+    componentDidMount() {
+        // document.addEventListener('productChanged', e => console.log('from search ', e.detail.id));
     }
 
     render(){
         return(
             <div>
-                <h1>oiii</h1>
-                <p>{this.state.input}</p>
-                <form>
+                <h1>Search</h1>
+                <form onSubmit={this.handleSubmit}>
                     <input type="text" onChange={this.handleChange}></input>
-                    <button Submit={this.handleSubmit}>click me</button>
+                    <button>search</button>
                 </form>
             </div>
         )
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('../db');
+// const db = require('../db');
 const path = require('path');
 const cors = require('cors');
 const app = express();
@@ -8,21 +8,21 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.use(express.json());
 app.use(cors());
 
-app.get('/items', (req, res) => {
-    //then query on change of the input box
-    db.findAllNames((err, suc) => {
-        if(err) console.log(err);
-        res.send(suc);
-    })
-})
+// app.get('/items', (req, res) => {
+//     //then query on change of the input box
+//     db.findAllNames((err, suc) => {
+//         if(err) console.log(err);
+//         res.send(suc);
+//     })
+// })
 
-app.post('/', (req, res) => {
-    //posting when clicking the item in the autofilling recommended
-    db.getSpecificName({username: req.body.name}, (err, results) => {
-        if(err) console.log('ERRRRR', err);
-        res.send(results);
-    })
-})
+// app.post('/', (req, res) => {
+//     //posting when clicking the item in the autofilling recommended
+//     db.getSpecificName({username: req.body.name}, (err, results) => {
+//         if(err) console.log('ERRRRR', err);
+//         res.send(results);
+//     })
+// })
 
 
 app.listen(3001, ()=> console.log(`shenanigans have started on aisle 3001`));
